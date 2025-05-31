202b13460893f4947afb09853a30eb65acd61305
bartek8b <163062773+bartek8b@users.noreply.github.com>
2025-05-31 17:37:35 +0200

    Add logic for auto rematch, to be click in UI

--- index.js
+++ index.js
@@ -53,25 +53,61 @@ function gamePlay(name1, name2) {
     const winner = checkWinner(gameBoard, activePlayer);
     if (!winner) {
       if (!gameBoard.includes(null)) {
-        console.log(`DRAW !!`);
         // REMIS !!!
+        console.log(`DRAW !!`);
+
+        //REMATCH SET
+        game = gamePlay(player2.name, player1.name);
       } else {
         activePlayer === player1
           ? (activePlayer = player2)
           : (activePlayer = player1);
       }
     } else {
-      console.log(`The winner is ${activePlayer.name}!!`);
       // WINNER !!!
+      console.log(`The winner is ${activePlayer.name}!!`);
+
+      //REMATCH SET
+      game = gamePlay(player2.name, player1.name);
     }
 
     return { gameBoard };
   };
 
-  return { gameBoard, playRound, activePlayer };
+  return { playRound };
 }
 
-const game = gamePlay("Bartonili", "Rival");
+let game = gamePlay("Bartonili", "Rival");
+
+console.log(game.playRound(8));
+console.log(game.playRound(5));
+console.log(game.playRound(2));
+console.log(game.playRound(4));
+console.log(game.playRound(3));
+console.log(game.playRound(1));
+console.log(game.playRound(7));
+console.log(game.playRound(0));
+console.log(game.playRound(6));
+
+console.log(game.playRound(8));
+console.log(game.playRound(5));
+console.log(game.playRound(2));
+console.log(game.playRound(4));
+console.log(game.playRound(3));
+console.log(game.playRound(1));
+console.log(game.playRound(7));
+console.log(game.playRound(0));
+console.log(game.playRound(6));
+
+console.log(game.playRound(8));
+console.log(game.playRound(5));
+console.log(game.playRound(2));
+console.log(game.playRound(4));
+console.log(game.playRound(3));
+console.log(game.playRound(1));
+console.log(game.playRound(7));
+console.log(game.playRound(0));
+console.log(game.playRound(6));
 
 console.log(game.playRound(8));
 console.log(game.playRound(5));
